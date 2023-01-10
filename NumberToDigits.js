5151d70f55d070eb2e1d605c71f498f18dc0960f
Kirubakaran Saravanan <93890704+kirubakaran-saravanan@users.noreply.github.com>
2023-01-10 20:39:25 +0530

    Add files via upload

---
+++ NumberToDigits.js
@@ -0,0 +1,12 @@
+
+
+/* Write a function that takes a number (a) as argument.
+Split a into its individual digits and return them in an array. */
+
+function myFunction(a) {
+    var digits = a.toString().split('');
+    var realDigits = digits.map(Number)
+    console.log(realDigits);
+  }
+
+  myFunction( 12345678 )
